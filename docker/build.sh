306b9045878616a8bf0365db32a0d465a5408512
Pascal Geiser <pgeiser@pgeiser.com>
2020-11-08 14:25:54 +0100

    Improve docker build.

--- docker/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -ex
-cd /mnt
-python3 -m pip wheel . -w dist
-
--- docker/html.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -ex
-cd /mnt/dist
-find . -type f -printf '[%P](%p)  \n' >index.md
-python -m markdown2 index.md >index.html
